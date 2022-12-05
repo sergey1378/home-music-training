1fbd9ed237f667febeac847ad9bba19c9c1b7176
p^sharapov31@gmail.com13nisharapov31@gmail.com^n78 <sharapov31@gmail.com>
2022-12-06 02:03:37 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import OpenSheetMusicDisplay from './lib/OpenSheetMusicDisplay'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // Don't call this.setState() here!
+    this.state = { file: "MuzioClementi_SonatinaOpus36No1_Part2.xml" };
+  }
+
+  handleClick(event) {
+    const file = event.target.value;
+    this.setState(state => state.file = file);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">OpenSheetMusicDisplay in React</h1>
+        </header>
+        <select onChange={this.handleClick.bind(this)}>
+          <option value="MuzioClementi_SonatinaOpus36No1_Part2.xml">Muzio Clementi: Sonatina Opus 36 No1 Part2</option>
+          <option value="Beethoven_AnDieFerneGeliebte.xml">Beethoven: An Die FerneGeliebte</option>
+        </select>
+        <OpenSheetMusicDisplay file={this.state.file} />
+      </div>
+    );
+  }
 }
 
 export default App;
